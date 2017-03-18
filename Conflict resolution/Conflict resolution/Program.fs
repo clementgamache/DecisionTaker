015a18ff098733ba6e61128f628b4b61a60b339a
Clement Gamache <clementgamache@gmail.com>
2017-03-17 21:58:03 -0400

    Tried to implement the algorithm in F# for practice purpose

---
+++ Conflict resolution/Conflict resolution/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let anInt = 5
+let aString = "Hello"
+// Perform a simple calculation and bind anIntSquared to the result.
+let anIntSquared = anInt * anInt
+
+let square n = n*n
+
+System.Console.WriteLine(anInt)
+System.Console.WriteLine(aString)
+System.Console.WriteLine(anIntSquared)
+System.Console.WriteLine(square anInt)
